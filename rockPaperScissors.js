a96ecaa57470c764a2180b824fe30bad8ab7baba
Art <mnsenna@protonmail.com>
2023-01-24 18:27:36 +1000

    COmplete rock paper scissors project

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,30 +11,47 @@ function getComputerChoice() {
     } else {
         computerChoice = 'scissors';
     }
-    console.log(computerChoice);
+    // console.log(computerChoice);
     return computerChoice
 }
 
 //const playerSelection = prompt("rock? paper? scissors?");
 //console.log(playerSelection);
 
-function playerSelect() {
-    const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase()
-}
+// const playerChoice = prompt("Rock, Paper, Scissors?").toLowerCase()
+
 function playRound(playerSelection, computerSelection) {
+    //console.log(`Player selection ${playerSelection}, Computer selection ${computerSelection}`)
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `Player wins. ${playerSelection} beats ${computerSelection}`;
+        return `Player wins.`;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `Player wins. ${playerSelection} beats ${computerSelection}`;
+        return `Player wins.`;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `Player wins. ${playerSelection} beats ${computerSelection}`;
+        return `Player wins.`;
     } else if (playerSelection == computerSelection) {
         return 'Tie!'
     } else {
-        return `Computer wins. ${computerSelection} beats ${playerSelection}`;
+        return `Computer wins.`;
     }
 }
 
-//getComputerChoice();
 
-playRound(playerSelect, getComputerChoice);
+function game() {
+    let playerScore = 0, computerScore = 0, tieScore = 0;
+    for (let i = 0; i < 2; i++) {
+        const playerChoice = prompt("Rock, Paper, Scissors?").toLowerCase()
+        let result = playRound(playerChoice, getComputerChoice());
+        if (result === `Player wins.`) {
+            playerScore++;
+        } else if (result === `Computer wins.`) {
+            computerScore++;
+        } else {
+            tieScore++;
+        }
+    }
+    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\nTie score: ${tieScore}`);
+}
+//playRound(playerChoice, getComputerChoice());
+
+game();
+
