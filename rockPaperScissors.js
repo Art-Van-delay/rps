4994c0fe20cfeabf18ccfe044ada14c957cf9396
Art <mnsenna@protonmail.com>
2023-02-01 17:34:46 +1000

    Complete rock paper scissors - revisited

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,9 +1,7 @@
-console.log("Source test");
-
 function getComputerChoice() {
     let numericalValue = Math.floor(Math.random() * 3);
     let computerChoice;
-    console.log(numericalValue);
+    //console.log(numericalValue);
     if (numericalValue == 0) {
         computerChoice = 'rock';
     } else if (numericalValue == 1) {
@@ -11,47 +9,90 @@ function getComputerChoice() {
     } else {
         computerChoice = 'scissors';
     }
-    // console.log(computerChoice);
     return computerChoice
 }
 
-//const playerSelection = prompt("rock? paper? scissors?");
-//console.log(playerSelection);
+const buttons = document.querySelectorAll('button');
+// Varaible set up
+let playerSelection = '';
+let computerSelection = '';
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
-// const playerChoice = prompt("Rock, Paper, Scissors?").toLowerCase()
 
+// DOM set up
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+container.appendChild(content);
+
+const pScore = document.createElement('p');
+pScore.textContent = `Player score: ${playerScore}`;
+container.append(pScore);
+
+const cScore = document.createElement('p');
+cScore.textContent = `Computer score: ${computerScore}`;
+container.append(cScore);
+
+const tScore = document.createElement('p');
+tScore.textContent = `Tie score: ${tieScore}`;
+container.append(tScore);
+
+
+
+
+// Event listener
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id.toString();
+        console.log(`Player selection: ${playerSelection}`)
+        console.log(`Computer selection: ${getComputerChoice()}`)
+        playRound(playerSelection, getComputerChoice());
+
+    });
+});
+
+// Play round logic
 function playRound(playerSelection, computerSelection) {
-    //console.log(`Player selection ${playerSelection}, Computer selection ${computerSelection}`)
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `Player wins.`;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `Player wins.`;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `Player wins.`;
-    } else if (playerSelection == computerSelection) {
-        return 'Tie!'
-    } else {
-        return `Computer wins.`;
-    }
-}
-
-
-function game() {
-    let playerScore = 0, computerScore = 0, tieScore = 0;
-    for (let i = 0; i < 2; i++) {
-        const playerChoice = prompt("Rock, Paper, Scissors?").toLowerCase()
-        let result = playRound(playerChoice, getComputerChoice());
-        if (result === `Player wins.`) {
+    if (playerScore <= 5 || computerScore <= 5 || tieScore <= 5) {
+        console.log(`Player selection ${playerSelection}, Computer selection ${computerSelection}`)
+        if (playerSelection === 'rock' && computerSelection === 'scissors') {
             playerScore++;
-        } else if (result === `Computer wins.`) {
-            computerScore++;
-        } else {
+            content.textContent = `${playerSelection} vs ${computerSelection}`;
+            console.log(`Player score: ${playerScore}`);
+            pScore.textContent = `Player score: ${playerScore}`;
+            return `Player wins.`;
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            playerScore++;
+            content.textContent = `${playerSelection} vs ${computerSelection}`;
+            console.log(`Player score: ${playerScore}`);
+            pScore.textContent = `Player score: ${playerScore}`;
+            return `Player wins.`;
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            playerScore++;
+            content.textContent = `Player: ${playerSelection} vs Computer: ${computerSelection}`;
+            console.log(`Player score: ${playerScore}`);
+            pScore.textContent = `Player score: ${playerScore}`;
+            return `Player wins.`;
+        } else if (playerSelection === computerSelection) {
             tieScore++;
+            content.textContent = `${playerSelection} vs ${computerSelection}`;
+            console.log(`Tie score: ${tieScore}`);
+            tScore.textContent = `Tie score: ${tieScore}`;
+            return 'Tie!'
+        } else {
+            computerScore++;
+            content.textContent = `Player: ${playerSelection} vs Computer: ${computerSelection}`;
+            cScore.textContent
+            console.log(`Computer score: ${computerScore}`);
+            cScore.textContent = `Computer score: ${computerScore}`;
+            return `Computer wins.`;
         }
-    }
-    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}\nTie score: ${tieScore}`);
-}
-//playRound(playerChoice, getComputerChoice());
 
-game();
+
+    }
+}
+
 
